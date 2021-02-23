a0e8fd83caf7d77481b3106c163f3ecb3309db02
Sumalyo <datudutta@gmail.com>
2021-02-24 02:21:18 +0530

    added functions

--- longprogram.c
+++ longprogram.c
@@ -1,3 +1,31 @@
 #include<stdio.h>
+#include<stdlib.h>
 //logical opertaor switch_case pointers arrays arguments preprocessors
 //a long program by gem...an ode to inefficient programming
+int main(int argc,char** argv)
+{   //the usual declaration for command line arguments in c
+    //this code accepts command line arguments amd does boring stuff
+    printf("Switch Calculator in C:\n Code presentation");
+    printf("use h for help [show this banner]\n");
+    printf("-----------------------------------------------------------\n");
+    printf("Usage ./program.exe [command] [params]\n");
+    printf("\t\t\t\t s : to display some system information\n");
+    printf("\t\t\t\t c [HEX_BYTE]: convert  0x00-0xFF to integer \n");
+    printf(" argv %d\n",argv[1][0]);
+
+    switch (argv[1][0])
+    {
+    case 's' /* constant-expression */:
+        /* code */
+        break;
+    case 'h':
+    default:
+    printf("Usage ./program.exe [command] [params]\n");
+    printf("\t\t\t\t s : to display some system information\n");
+    printf("\t\t\t\t c [HEX_BYTE]: convert  0x00-0xFF to integer\n");
+
+        break;
+    }
+    return 0;
+
+}
