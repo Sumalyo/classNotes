fcf2b1fa9f52a12528a6c4da0c970e2983daf70f
Sumalyo <datudutta@gmail.com>
2021-02-24 10:35:59 +0530

    Final changes

--- longprogram.c
+++ longprogram.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+//compilation command gcc longprogram.c -o program
 int main(int argc,char** argv)
 {   //the usual declaration for command line arguments in c
     //this code accepts command line arguments amd does boring stuff
@@ -25,9 +26,12 @@ int main(int argc,char** argv)
     printf("-----------------------------------\n");
     printf("Current user: %s \n",getenv("USERNAME"));
     break;
+
     case 'c':
-   
-    switch(argv[2][0])
+    switch(argc)
+    {
+        case 3:
+    switch(argv[2][1])
     {
         case '0':x+=0; break;
         case '1':x+=1; break;
@@ -46,7 +50,7 @@ int main(int argc,char** argv)
         case 'E': case 'e':x+=14; break;
         case 'F': case 'f':x+=15;break;
     }
-        switch(argv[2][1])
+        switch(argv[2][0])
     {
         case '0':x+=0; break;
         case '1':x+=16; break;
@@ -67,36 +71,84 @@ int main(int argc,char** argv)
     }
     printf("The decimal value of %s is %d",argv[2],x);
     break;
+    default:
+    printf("Invalid params present. Check usage\n");
+    printf("\t\t\t\t c [HEX_BYTE]: convert  0x00-0xFF to integer \n");
+    }
+    break;
+
     case 'a':
+    switch (argc)
+    {
+    case 4:
     y1 =(int)strtol(argv[2],NULL,16);
     y2 = (int)strtol(argv[3],NULL,16);
     y = y1+y2;
     itoa(y,tmp,16);
     printf("The sum is %s tmp (decimal value %d)",tmp,y);
     break;
-       case 'p':
+    default:
+    printf("Invalid params present. Check Usage\n");
+    printf("\t\t\t\t a [HEX] [HEX]: to display the sum of any two hex expressions\n");
+    }
+    break;
+
+
+    case 'p':
+    switch (argc)
+    {
+    case 4:
     y1 =(int)strtol(argv[2],NULL,16);
     y2 = (int)strtol(argv[3],NULL,16);
     y = y1*y2;
     itoa(y,tmp,16);
-    printf("The sum is %s tmp (decimal value %d)",tmp,y);
+    printf("The product is %s tmp (decimal value %d)",tmp,y);
     break;
-       case 'm':
+    default:
+    printf("Invalid params present. Check Usage\n");
+    printf("\t\t\t\t p [HEX] [HEX]: to display the product of any two hex expressions\n");
+    }
+    break;
+    case 'm':
+    switch (argc)
+    {
+    case 4:
     y1 =(int)strtol(argv[2],NULL,16);
     y2 = (int)strtol(argv[3],NULL,16);
     y = y1-y2;
     itoa(y,tmp,16);
-    printf("The sum is %s tmp (decimal value %d)",tmp,y);
+    printf("The differnce is %s tmp (decimal value %d)",tmp,y);
     break;
+    default:
+    printf("Invalid params present. Check Usage\n");
+    printf("\t\t\t\t m [HEX] [HEX]: to display the difference of any two hex expressions\n");
+    }
+    break;
+
+
+
+
 
     case 'h':
     default:
     printf("Usage ./program.exe [command] [params]\n");
     printf("\t\t\t\t s : to display some system information\n");
     printf("\t\t\t\t c [HEX_BYTE]: convert  0x00-0xFF to integer\n");
+    printf("\t\t\t\t a [HEX] [HEX]: to display the sum of any two hex expressions\n");
+    printf("\t\t\t\t p [HEX] [HEX]: to multiply two HEX expressions\n");
+    printf("\t\t\t\t m [HEX1] [HEX2]: to substract two HEX expressions HEX1 - HEX2\n\n");
 
-        break;
+    break;
     }
     return 0;
-
+/* 
+This program aims to show some capabilities of C programming especially 
+in the use of command line args and switch case. The aim here is to mainly
+work with HEX Bytes and expressions which are quite commonly dealt with by
+programmers during analysis of low level machine code etc. This also shows 
+some very useful fuctions that are inbulit in C in the stdlib.h header file
+that help it know some information about the environment it resides in.
+NOTE: THIS IS TO BE ONLY COMPILED AND RUN ON WINDOWS. Many components here
+are OS Specific. 
+*/
 }
